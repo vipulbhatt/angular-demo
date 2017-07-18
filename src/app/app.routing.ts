2b4d3db199df4ade7dc3d30a93387c6a0b9dac43
Vipul Bhatt <vipul.b@droisys.com>
2017-07-18 18:09:39 +0530

    first commit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -12,7 +12,7 @@ const routes:Routes = [
                 path:'',
                 loadChildren: './home/home.module#HomeModule'
              },
-             {
+             { 
                 path:'users',
                 loadChildren: './users/users.module#UsersModule'
              },
