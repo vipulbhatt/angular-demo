5dac12464c09570fed21a323ccdcfc6158880f17
Vipul Bhatt <vipul.b@droisys.com>
2017-07-18 17:54:01 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { SidebarModule } from 'ng-sidebar';
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app.routing';
+import {LayoutComponent} from './layout/layout.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+//import{UsersComponent} from './users/users.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SidebarModule.forRoot(),
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
